4ce13981ad84047f6cd44377e0b7e71f31efe643
skkim125 <kthanks125@gmail.com>
2025-01-22 21:00:34 +0900

    프로젝트 생성

---
+++ TodaySpace/TodaySpace/TodaySpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodaySpaceApp.swift
+//  TodaySpace
+//
+//  Created by 김상규 on 1/22/25.
+//
+
+import SwiftUI
+
+@main
+struct TodaySpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
