8b55870d139aa445902486fa4b25ce25eaa49d17
skkim125 <kthanks125@gmail.com>
2025-02-23 17:21:48 +0900

    [Feat] 이미지 다운로드 시 캐싱 추가

--- TodaySpace/TodaySpace/Network/NetworkManager.swift
+++ TodaySpace/TodaySpace/Network/NetworkManager.swift
@@ -46,50 +46,32 @@ final class NetworkManager {
     }
     
     func fetchImage(imageURL: String) async throws -> UIImage {
+        if let cachedImage = ImageCacher.shared.image(forKey: imageURL) {
+            print("캐싱된 이미지")
+            return cachedImage
+        }
+        
         guard let url = URL(string: API.baseURL + "/v1/" + imageURL) else {
             throw NetworkError.invalidURL
         }
-        var request = URLRequest(url: url)
         
+        var request = URLRequest(url: url)
         request.setValue(API.productId, forHTTPHeaderField: Header.productId)
         request.setValue(UserDefaultsManager.accessToken, forHTTPHeaderField: Header.authorization)
         request.setValue(API.apiKey, forHTTPHeaderField: Header.sesacKey)
         
-        do {
-            let (data, response) = try await URLSession.shared.data(for: request)
-            
-            guard let httpResponse = response as? HTTPURLResponse else {
-                throw NetworkError.invalidResponse
-            }
-            
-            guard httpResponse.statusCode == 200 else {
-                print(httpResponse.statusCode)
-                if httpResponse.statusCode == 419 {
-                    do {
-                        try await tokenRefresh()
-                        return try await fetchImage(imageURL: imageURL)
-                    }
-                } else {
-                    do {
-                        let errorResponse = try JSONDecoder().decode(ErrorType.self, from: data)
-                        print(errorResponse.message)
-                        throw errorResponse
-                    } catch {
-                        throw NetworkError.checkNetworkError(errorCode: httpResponse.statusCode)
-                    }
-                }
-            }
-            
-            guard let image = UIImage(data: data) else {
-                throw NetworkError.decodingError
-            }
-            
-            return image
-        } catch {
-            print("이미지 로드 실패")
-            
-            throw error
+        let (data, response) = try await URLSession.shared.data(for: request)
+        
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw NetworkError.invalidResponse
         }
+        
+        guard let image = UIImage(data: data) else {
+            throw NetworkError.decodingError
+        }
+        
+        ImageCacher.shared.insertImage(image, forKey: imageURL)
+        return image
     }
     
     func tokenRefresh() async throws {
---
+++ TodaySpace/TodaySpace/Resource/ImageCacher.swift
@@ -0,0 +1,31 @@
+//
+//  ImageCacher.swift
+//  TodaySpace
+//
+//  Created by 김상규 on 2/23/25.
+//
+
+import SwiftUI
+
+final class ImageCacher {
+    static let shared = ImageCacher()
+    private init() {}
+
+    private let cache = NSCache<NSString, UIImage>()
+
+    func image(forKey key: String) -> UIImage? {
+        return cache.object(forKey: key as NSString)
+    }
+
+    func insertImage(_ image: UIImage?, forKey key: String) {
+        guard let image = image else {
+            removeImage(forKey: key)
+            return
+        }
+        cache.setObject(image, forKey: key as NSString)
+    }
+
+    func removeImage(forKey key: String) {
+        cache.removeObject(forKey: key as NSString)
+    }
+}
